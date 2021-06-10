299c7d71c5a2071b12888013789f8539f2d26f5c
Kahlil B <kahlilbeasley@Kahlils-MacBook-Air.local>
2021-06-09 21:03:19 -0700

    added env vars

--- Golang/go-sample-web-app/cmd/app/main.go
+++ Golang/go-sample-web-app/cmd/app/main.go
@@ -3,17 +3,35 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Error var - used for checking env/server etc.
+	var err error
+
+	// Load env variables
+	err = godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env vars.")
+	}
+
+	// Get PORT env var if empty manually set it
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = ":8080"
+	}
 
 	serve := &http.Server{
-		Addr:    ":8080",
+		Addr:    PORT,
 		Handler: routes(),
 	}
 	// Start server
-	log.Println("Starting server ... on port 8080")
-	err := serve.ListenAndServe()
+	log.Printf("Starting server ... on port - %s", PORT)
+
+	err = serve.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
