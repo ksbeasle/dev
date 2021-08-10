c46732830a1d4750deec66117da0ff1f6c79511b
Kahlil Beasley <ksbeasle@asu.edu>
2021-08-09 20:55:14 -0700

    chai/hooks

--- NodeJS/PSTestingNodeWithMocha/app/parts.spec.js
+++ NodeJS/PSTestingNodeWithMocha/app/parts.spec.js
@@ -1,5 +1,14 @@
+const expect = require('chai').expect;
+const should = require('chai').should();
+const assert = require('chai').assert;
+
 describe('Some More Tests', () =>{
     it('Pass some', () => {
+        let result = true
+
+        expect(result).to.be.true;
+        result.should.be.true;
+        assert.isTrue(result);
         
     })
 })
